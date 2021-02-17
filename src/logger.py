d3e7b00930b9ac49a32dc0e613f35403552e05e6
mperini <marco.perini@ibm.com>
2021-02-17 10:55:25 +0100

    bugfix loggingTests

--- src/logger.py
+++ src/logger.py
@@ -7,6 +7,9 @@ from datetime import date
 if not os.path.exists(os.path.join(".", "logs")):
     os.mkdir("logs")
 
+module_path = os.path.abspath(__file__)
+dir_path = os.path.dirname(module_path)
+unittest_path = os.path.join(dir_path, "..", "unittests")
 
 def update_train_log(data_shape, runtime, MODEL_VERSION, MODEL_VERSION_NOTE, test):
     """
@@ -18,7 +21,7 @@ def update_train_log(data_shape, runtime, MODEL_VERSION, MODEL_VERSION_NOTE, tes
     if test:
         logfile = os.path.join("logs", "train-test.log")
     else:
-        logfile = os.path.join("logs", "train-{}-{}.log".format(today.year, today.month))
+        logfile = os.path.join(unittest_path, "logs", "train-{}-{}.log".format(today.year, today.month))
 
     ## write the data to a csv file
     header = ['unique_id', 'timestamp', 'x_shape', 'model_version',
@@ -46,7 +49,7 @@ def update_predict_log(y_pred, runtime, MODEL_VERSION, MODEL_VERSION_NOTE, test)
     if test:
         logfile = os.path.join("logs", "predict-test.log")
     else:
-        logfile = os.path.join("logs", "predict-{}-{}.log".format(today.year, today.month))
+        logfile = os.path.join(unittest_path, "logs", "predict-{}-{}.log".format(today.year, today.month))
 
     ## write the data to a csv file
     header = ['unique_id', 'timestamp', 'y_pred', 'y_proba', 'query', 'model_version', 'runtime']
--- unittests/LoggingTests.py
+++ unittests/LoggingTests.py
@@ -11,8 +11,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', "src"))
 ## import model specific functions and variables
 from logger import update_train_log, update_predict_log
 
-LOG_DIR = os.path.join(os.path.dirname(__file__), '..', 'log')
-LOG_PREFIX = 'unittests'
+LOG_DIR = os.path.join(os.path.dirname(__file__), '..', 'unittests', 'logs')
+#LOG_DIR = os.path.join(os.path.dirname(__file__), 'logs')
 
 
 class LoggingTest(unittest.TestCase):
@@ -25,20 +25,18 @@ class LoggingTest(unittest.TestCase):
         ensure log file is created
         """
         today = date.today()
-        log_file = os.path.join(LOG_DIR, "{}-train-{}-{}.log".format(LOG_PREFIX, today.year, today.month))
+        log_file = os.path.join(LOG_DIR, "train-{}-{}.log".format(today.year, today.month))
         if os.path.exists(log_file):
             os.remove(log_file)
 
         ## update the log
-        country = 'india'
         date_range = ('2017-11-29', '2019-05-24')
-        metric = {'rmse': 0.5}
         runtime = "00:00:01"
         model_version = 0.1
         model_version_note = "test model"
 
-        update_train_log(country, date_range, metric, runtime,
-                         model_version, model_version_note, test=True, prefix=LOG_PREFIX)
+        update_train_log(date_range, runtime,
+                         model_version, model_version_note, test=False)
 
         self.assertTrue(os.path.exists(log_file))
 
@@ -47,42 +45,37 @@ class LoggingTest(unittest.TestCase):
         ensure that content can be retrieved from log file
         """
         today = date.today()
-        log_file = os.path.join(LOG_DIR, "{}-train-{}-{}.log".format(LOG_PREFIX, today.year, today.month))
+        log_file = os.path.join(LOG_DIR, "train-{}-{}.log".format(today.year, today.month))
 
         ## update the log
-        country = 'india'
         date_range = ('2017-11-29', '2019-05-24')
-        metric = {'rmse': 0.5}
         runtime = "00:00:01"
         model_version = 0.1
         model_version_note = "test model"
 
-        update_train_log(country, date_range, metric, runtime,
-                         model_version, model_version_note, test=True, prefix=LOG_PREFIX)
+        update_train_log(date_range, runtime,
+                         model_version, model_version_note, test=False)
 
         df = pd.read_csv(log_file)
-        logged_metric = [literal_eval(i) for i in df['metric'].copy()][-1]
-        self.assertEqual(metric, logged_metric)
+        logged_model_version = df["model_version"].iloc[-1]
+        self.assertEqual(model_version, logged_model_version)
 
     def test_03_predict(self):
         """
         ensure log file is created
         """
         today = date.today()
-        log_file = os.path.join(LOG_DIR, "{}-predict-{}-{}.log".format(LOG_PREFIX, today.year, today.month))
+        log_file = os.path.join(LOG_DIR, "predict-{}-{}.log".format(today.year, today.month))
         if os.path.exists(log_file):
             os.remove(log_file)
 
         ## update the log
         y_pred = [0]
-        y_proba = [0.6, 0.4]
         runtime = "00:00:02"
         model_version = 0.1
-        country = "india"
-        target_date = '2018-01-05'
 
-        update_predict_log(country, y_pred, y_proba, target_date, runtime,
-                           model_version, test=True, prefix=LOG_PREFIX)
+        update_predict_log(y_pred, runtime,
+                           model_version, None, test=False)
 
         self.assertTrue(os.path.exists(log_file))
 
@@ -91,22 +84,19 @@ class LoggingTest(unittest.TestCase):
         ensure that content can be retrieved from log file
         """
         today = date.today()
-        log_file = os.path.join(LOG_DIR, "{}-predict-{}-{}.log".format(LOG_PREFIX, today.year, today.month))
+        log_file = os.path.join(LOG_DIR, "predict-{}-{}.log".format(today.year, today.month))
 
         ## update the log
         y_pred = [0]
-        y_proba = [0.6, 0.4]
         runtime = "00:00:02"
         model_version = 0.1
-        country = "india"
-        target_date = '2018-01-05'
 
-        update_predict_log(country, y_pred, y_proba, target_date, runtime,
-                           model_version, test=True, prefix=LOG_PREFIX)
+        update_predict_log(y_pred, runtime,
+                           model_version, None, test=False)
 
         df = pd.read_csv(log_file)
-        logged_y_pred = [literal_eval(i) for i in df['y_pred'].copy()][-1]
-        self.assertEqual(y_pred, logged_y_pred)
+        logged_y_pred = df['y_pred'].iloc[-1]
+        self.assertEqual(str(y_pred), logged_y_pred)
 
 
 ### Run the tests
